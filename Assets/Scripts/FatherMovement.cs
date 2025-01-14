30a703408454cc343e24177288cc4b8dcff4c5c3
DacPhu <87571273+DacPhu@users.noreply.github.com>
2025-01-14 21:43:55 +0700

    Update Father

--- Assets/Scripts/FatherMovement.cs
+++ Assets/Scripts/FatherMovement.cs
@@ -8,35 +8,57 @@ public class FatherMovement : MonoBehaviour
     public Transform Target;
     public float UpdateSpeed = 0.1f;
     public float ChaseDistance = 10f;
-    public AudioSource SoundSource; 
-    List <Transform> CheckPoints = new List <Transform>();
+    public AudioSource SoundSource;
+    public List<Transform> Checkpoints = new List<Transform>();
 
+    private int CurrentCheckpointIndex = 0;
+    private float ReachCheckpointThreshold = 2f;
     private NavMeshAgent Agent;
+    private Animator Animator;
 
     private void Awake()
     {
         Agent = GetComponent<NavMeshAgent>();
+        Animator = GetComponent<Animator>();
     }
 
     public void Start()
     {
-        StartCoroutine(FollowTarget());
+        StartCoroutine(FollowRoute());
     }
 
-    private IEnumerator FollowTarget()
+    private IEnumerator FollowRoute()
     {
-        WaitForSeconds Wait = new WaitForSeconds(UpdateSpeed);
-        while(enabled)
+        WaitForSeconds wait = new WaitForSeconds(UpdateSpeed);
+
+        while (enabled)
         {
+            float distance = Vector3.Distance(Target.position, transform.position);
+            if (distance <= ReachCheckpointThreshold)
+            {
+                Animator.SetInteger("State", 0); // Chased
+            }
+            else
+            {
+                Animator.SetInteger("State", 1); // Walking
+            }
             if (ShouldChase())
             {
                 Agent.SetDestination(Target.position);
             }
             else
             {
-                Agent.SetDestination(transform.position);
+                Transform currentCheckpoint = Checkpoints[CurrentCheckpointIndex];
+                Agent.SetDestination(currentCheckpoint.position);
+
+                float distanceToCheckpoint = Vector3.Distance(transform.position, currentCheckpoint.position);
+                if (distanceToCheckpoint <= ReachCheckpointThreshold)
+                {
+                    CurrentCheckpointIndex = (CurrentCheckpointIndex + 1) % Checkpoints.Count;
+                }
             }
-            yield return Wait;
+
+            yield return wait;
         }
     }
 
--- Assets/Scripts/IDamageable.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public interface IDamageable
-{
-    void TakeDamage(int Damage);
-
-    Transform GetTransform();
-}
